4dfd5e781aa0d2856cbb305476b2e0bfc6bf6ed3
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-16 20:47:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,10 +63,10 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#    :languages => ["LISP, C"]
 # }
 
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -82,12 +82,15 @@ def adding_matz
         }
      }
 
-  programmer_hash = [:yukihiro_matsumoto] => {
-    :known_for => "Ruby", 
-    :languages => ["LISP", "C"]
-  }
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
+    
   programmer_hash
 end
+
 
 def changing_alan
 	programmer_hash = 
